e743701f2fde9f86aef1e8de8f4e16bd53a64a94
guang <aylydzgy@163.com>
2022-03-10 21:09:13 +0800

    Site updated: 2022-03-10 21:09:09

---
+++ js/weather.js
@@ -0,0 +1,25 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "FFFFFF",
+    "tmpSize": "16",
+    "cityColor": "FFFFFF",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "borderRadius": "5",
+    "fixed": "true",
+    "vertical": "top",
+    "horizontal": "left",
+    "city": "CN101070101",
+    "left": "125",
+    "top": "10",
+    "key": "83c4958c657c4784a76df281a9bcecf5"
+  }
+}
